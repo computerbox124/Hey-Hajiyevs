68d7c72ec47b192c6beac78af9b834b32db78974
computerbox <computerbox124@gmail.com>
2021-03-06 19:40:16 +0400

    Added sender and receiver

---
+++ sender.py
@@ -0,0 +1,19 @@
+import requests
+
+# response = requests.get(
+#     'http://127.0.0.1:5000/status'
+# )
+# print(response)
+# print(response.status_code)
+# print(response.headers)
+# print(response.text)
+# print(response.json()['name'])
+
+name = input('Input your name: ')
+
+while True:
+    text = input()
+    response = requests.post(
+        'http://127.0.0.1:5000/send',
+        json={'name': name, 'text': text}
+    )
